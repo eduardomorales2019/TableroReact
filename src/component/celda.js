c63091bd2cbd83681982a772defeb642176f4073
Eduardo_Morales <eduardo@inmo-puebla.com>
2022-02-24 22:52:30 -0600

    creacion de componene tablero, el cual determina la base de este ejercicio. en donde  el state creal tablero, gracias a la  funcion de createboard(), que es una iteracion de eje x y y para que sea mas facil la identificion y no se nececiten keys.

--- src/component/celda.js
+++ src/component/celda.js
@@ -3,12 +3,22 @@ import "../estilos/celda.css";
 class Celda extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { color: "none" };
+    this.handlerClick = this.handlerClick.bind(this);
   }
+
+  handlerClick = () => {
+    // here  i will call up the board to flip cells around this cell.
+    // this.props.flipcellAroundme()  --- aqui iria este metodo al momento de hacer click cambia color.
+  };
+  //   ============
   render() {
+    // dependieng on variable is gonna lit  with the class
+    let classes = "Cell" + (this.props.isLit ? "cell-lit" : "");
     return (
       <div>
-        <p>{this.state.color}</p>
+        <td className="aquilavariable" onClick={this.handlerClick}>
+          aqui va un estado
+        </td>
       </div>
     );
   }
--- src/component/tablero.js
+++ src/component/tablero.js
@@ -1,39 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import "../estilos/tablero.css";
-import Celda from "./celda";
+// import Celda from "./celda";
 
 // most important   component is these one!
 
-class Tablero extends React.Component {
+class Tablero extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightsStarOn: 0.25,
+  };
+
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      hasWon: false,
+      board: this.createBoard(),
+    };
+    console.log(props, "Soy props");
+    console.log(this.state.board);
   }
+  //   ================
+  createBoard() {
+    let board = [];
+    // TODO: create array-of-arrays of true/false values
+    //  fill in all cells with a random `true` or `false` value
+    //   - use `this.props.chanceLightStartsOn` to determine which cells are lit at start of game (use Math.random)  ...
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightsStarOn);
+      }
+      board.push(row);
+      console.log(row, "soy Row ");
+    }
+    return board;
+  }
+
+  //   flipsCellAround = (coordenada) => {
+  //     let { ncols, nrows } = this.props;
+  //     let board = this.state.board;
+  //     // let [x, y] = coordenada.split.map(Numero);
+
+  //     // function flipCell(y, x) {
+  //     //   if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+  //     //     board[y][x] = !board[y][x];
+  //     //   }
+  //     // }
+  //   };
+
+  //   ================
   render() {
     return (
       <div className="tablero">
         <h1>Tablero </h1>
-        <div className="Container ">
-          <table>
-            <tr>
-              <th>
-                <Celda />
-              </th>
-              <th>
-                <Celda />
-              </th>
-              <th>
-                <Celda />
-              </th>
-              <th>
-                <Celda />
-              </th>
-              <th>
-                <Celda />
-              </th>
-            </tr>
-          </table>
-        </div>
       </div>
     );
   }
